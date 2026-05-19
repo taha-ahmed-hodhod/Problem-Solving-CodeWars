f895810f6a9607d078ba3a07b652d7b45253c66c
taha ahmed <tahahodhod650@gmail.com>
2026-05-19 04:32:29 +0300

    challenge 4

---
+++ Repeat-string.js
@@ -0,0 +1,6 @@
+//Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+function repeatString(str, n){
+    return str.repeat(n);
+}
+
+console.log(repeatString("taha", 5))
